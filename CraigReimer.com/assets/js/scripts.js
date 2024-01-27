4241504fa5805efc70b9fe6eb95d6d0ae6388403
Craig Reimer <500145211@fvtc.edu>
2024-01-27 09:22:04 -0600

    More CSS

--- CraigReimer.com/assets/js/scripts.js
+++ CraigReimer.com/assets/js/scripts.js
@@ -144,7 +144,7 @@ window.onload = function () {
     // build the Exposure Icons
     for (let i = 0; i < exposureIcons.length; i++) {
         let icon = document.createElement("div");
-        icon.classList.add("custom-card", "justify-content-center");
+        icon.classList.add("icon-card", "justify-content-center");
 
         let a = document.createElement("a");
         a.href = exposureIcons[i].href;
@@ -170,7 +170,7 @@ window.onload = function () {
     // build the Connect Icons
     for (let i = 0; i < connectIcons.length; i++) {
         let icon = document.createElement("div");
-        icon.classList.add("custom-card", "justify-content-center");
+        icon.classList.add("icon-card", "justify-content-center");
 
         let a = document.createElement("a");
         a.href = connectIcons[i].href;
