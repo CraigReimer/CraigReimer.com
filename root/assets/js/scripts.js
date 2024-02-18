b37fdc76c3b8988e5ecd5ddd1a90b824895e32f7
CraigReimer <121911600+CraigReimer@users.noreply.github.com>
2024-02-17 22:44:37 -0600

    icon-card updates, verbiage updates

--- root/assets/js/scripts.js
+++ root/assets/js/scripts.js
@@ -17,8 +17,8 @@ const shortBioParagraphs = [
 
 const longBioParagraphs = [
     "I am a second year undergraduate student at Fox Valley Technical College in Appleton, Wisconsin. I am pursuing an AAS in Software Development. I am passionate about learning new things and creating apps that empower people.",
-    "I originally started learning HTML and wrote my first webpage while in highschool, but left it behind when I joined the Army. I held a latent interest in Computer Science for a number of years, and in 2022 made the decision to return to school and restart my coding journey.",
-    "This website is hosted on Amazon S3. It is a work in progress, and probably always will be, just like me. As I gain new skills, I will employ and hone them here. Once I have a project worth sharing, I will add it here as well.",
+    "I originally started learning HTML and wrote my first webpage while in highschool, but left it behind when I joined the Army. I held a latent interest in Computer Science for a number of years, and in 2022 made the decision to return to school and restart my developing journey.",
+    "This website is hosted on Amazon S3 via GitHub and GitHub Actions. It is a work in progress, and probably always will be, just like me. As I gain new skills, I will employ and hone them here.",
     "My first app was a simple punchclock that I still use to log my hours while working from home. It is a C# WinForms App and uses the .NET Framework. You can check it out on my Github. The practical experience of programming this app helped solidify my understanding of OOP and n-tier architecture, and gave me the great satisfaction of solving my own needs through software.",
     "Thank you for taking the time to read through!",
 ];
@@ -96,6 +96,24 @@ const exposureIcons = [
         text: "C#",
     },
     {
+        href: "https://dotnet.microsoft.com/en-us/",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dot-net/dot-net-original-wordmark.svg",
+        imgAlt: ".NET Logo",
+        text: ".NET",
+    },
+    {
+        href: "https://dotnet.microsoft.com/en-us/apps/xamarin",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/xamarin/xamarin-original.svg",
+        imgAlt: "Xamarin Logo",
+        text: "Xamarin",
+    },
+    {
+        href: "https://developer.apple.com/xcode/",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/xcode/xcode-original.svg",
+        imgAlt: "Xcode Logo",
+        text: "Xcode",
+    },
+    {
         href: "https://learn.microsoft.com/en-us/sql/?view=sql-server-ver16",
         imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
         imgAlt: "MySQL Logo",
@@ -106,6 +124,30 @@ const exposureIcons = [
         imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
         imgAlt: "Git Logo",
         text: "Git",
+    },
+    {
+        href: "https://bitbucket.org/",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bitbucket/bitbucket-original-wordmark.svg",
+        imgAlt: "Bitbucket Logo",
+        text: "Bitbucket",
+    },
+    {
+        href: "https://azure.microsoft.com/en-us/products/devops/",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuredevops/azuredevops-original.svg",
+        imgAlt: "Azure DevOps Logo",
+        text: "Azure DevOps",
+    },
+    {
+        href: "https://github.com/craigreimer",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+        imgAlt: "GitHub Logo",
+        text: "GitHub",
+    },
+    {
+        href: "https://docs.github.com/en/actions",
+        imgSrc: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/githubactions/githubactions-original.svg",
+        imgAlt: "Github Actions Logo",
+        text: "Github Actions",
     },
     {
         href: "https://aws.amazon.com/",
